b585f968098f1d6bf7779ca0898f539992f0fb81
AChapman <achapman77@gmail.com>
2019-05-09 23:51:49 -0600

    Created Blog excerpts page

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,47 +13,35 @@
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h3 class="entry-title">', '</h3>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				bootstrap2wordpress_posted_on();
-				bootstrap2wordpress_posted_by();
-				?>
-			</div><!-- .entry-meta -->
+		if ( 'post' === get_post_type() ) : ?>
+			<div class="post-details">
+				<i class="fas fa-user"></i> <?php the_author(); ?>
+				<i class="fas fa-clock"></i> <?php the_date(); ?>
+				<i class="fas fa-folder"></i> <?php the_category(', '); ?>
+				<i class="fas fa-tags"></i> <?php the_tags('', ', ', ''); ?>
+
+				<div class="post-comments-badge">
+					<a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?>
+					</a>
+				</div><!-- post-comments-badge -->
+				<?php edit_post_link( 'Edit', '<i class="fas fa-pencil-alt"></i>', '' ); ?>
+			</div><!-- post-details -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
+	<?php if ( has_post_thumbnail() ) { //check for feature image ?>
+		<div class="post-image">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/hero-bg.jpg">
+		</div><!-- post-image -->
+	<?php } ?>
+	<div class="post-excerpt">
+		<?php the_excerpt(); ?>
+	</div><!-- post-excerpt -->
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bootstrap2wordpress' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	
 </article><!-- #post-<?php the_ID(); ?> -->
