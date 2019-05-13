e4f79ad09e8690b75d5faa057f13263307dd5022
AChapman <achapman77@gmail.com>
2019-05-12 20:16:26 -0600

    Hooked up single post page

--- single.php
+++ single.php
@@ -7,31 +7,36 @@
  * @package bootstrap2wordpress
  */
 
-get_header();
-?>
+get_header(); ?>
+	<!-- BLOG CONTENT
+			================================================== -->
+	<div class="container">
+		<div class="row" id="primary">
+  
+			<main id="content" class="col-sm-8">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					<?php get_template_part( 'template-parts/content-single', get_post_type() ); ?>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+					<?php the_post_navigation(); ?>
 
-			the_post_navigation();
+					<!-- If comments are open or we have at least one comment, load up the comment template. -->
+					<?php if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif; ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				<?php endwhile; ?>
 
-		endwhile; // End of the loop.
-		?>
+			</main><!-- #content -->
+			
+			<!-- SIDEBAR
+			================================================== -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+		
+		</div><!-- #primary -->
+	</div><!-- .container -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
