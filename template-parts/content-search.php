733a5209352497ae29466f9058406f9ed2abe43a
AChapman <achapman77@gmail.com>
2019-05-12 21:54:50 -0600

    Hooked up and styled search feature

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,25 +11,37 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		endif;
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			bootstrap2wordpress_posted_on();
-			bootstrap2wordpress_posted_by();
-			?>
-		</div><!-- .entry-meta -->
+		if ( 'post' === get_post_type() ) : ?>
+			<div class="post-details">
+				<i class="fas fa-user"></i> <?php the_author(); ?>
+				<i class="fas fa-clock"></i> <?php the_date(); ?>
+				<i class="fas fa-folder"></i> <?php the_category(', '); ?>
+				<i class="fas fa-tags"></i> <?php the_tags('', ', ', ''); ?>
+
+				<div class="post-comments-badge">
+					<a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?>
+					</a>
+				</div><!-- post-comments-badge -->
+				<?php edit_post_link( 'Edit', '<i class="fas fa-pencil-alt"></i>', '' ); ?>
+			</div><!-- post-details -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
-
-	<div class="entry-summary">
+	<?php if ( has_post_thumbnail() ) { //check for feature image ?>
+		<div class="post-image">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/hero-bg.jpg">
+		</div><!-- post-image -->
+	<?php } ?>
+	<div class="post-excerpt">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	</div><!-- post-excerpt -->
 
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	
 </article><!-- #post-<?php the_ID(); ?> -->
